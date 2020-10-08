f254f6fa757a93bc7f35aecf10f2b89d661ede91
kerroli <70635088+kerroli@users.noreply.github.com>
2020-10-08 23:01:53 +0300

    Update 1lab.cpp

--- 1lab.cpp
+++ 1lab.cpp
@@ -1,76 +1,80 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 
 int task1();
 int task2();
 int task3();
 int task4();
-int task5();
 
 int main() {
-    std::cout << "Hi there" << std::endl;
     task1();
     task2();
     task3();
     task4();
-    task5();
     return 0;
 }
-// 1
-int task1() {
-    int x; // min -2 147 483 648, max 2 147 483 647, -2^31 ; 2^31-1
-    std::cout << "First task, insert x:\n";
-    std::cin >> x;
-    std::cout << std::fixed << std::setprecision(3) << "Answer: "<< pow(x,6)-5*pow(x,5)-1 << std::endl;
+
+int task1(){
+    std::cout << "First task:" << std::endl;
+    for(int y = 0 ; y <= 255 ; y++)
+        std::cout << y << ": " << static_cast<char>(y) << std::endl;
     return 0;
 }
-// 2
+
 int task2() {
-    unsigned int x; // min 0, max 4 294 967 295, 2^32-1
-    std::cout << "Second task, insert a:\n";
+    std::cout << "Second task:" << std::endl;
+    char x;
+    std::cout << "Enter your symbol:" << std::endl;
     std::cin >> x;
-    if (x<1)
-    {
-        std::cout << "No Answer\nSqrt can not be <0\n";
+    if (48 <= x && x <= 57){
+        std::cout << "Entered symbol is a digit" << std::endl;
         return 0;
     }
-    std::cout << std::setprecision(0) << std::showpos << "Answer: "<< (x+2) + sqrt(x-1) << std::endl; // abs(x+2) ne nuzhno tk odz x>=1
-    return 0;
-}
-// 3
-int task3() {
-    bool n, l, m, k;
-    bool answ;
-    std::cout << "Third task, insert n, l, m, k:\n";
-    std::cin >> n >> l >> m >> k;
-    answ = (n&&l||(m^!k));
-    std::cout<< "Answer: "<< std::boolalpha << answ  << std::endl;
-    return 0;
-}
-// 4
-int task4() {
-    unsigned short int x;  // min 0, max 65535, 2^16-1
-    int answ;
-    std::cout << "Fourth task, insert b:\n";
-    std::cin >> x;
-    answ = pow(2,x);
-    if ( x >= 0 && x <= 7 ) {
-        std::cout << std::noshowpos << "Answer: " << answ << " ";
-        std::cout << std::oct << answ << std::endl;
+    if ((65 <= x && x <= 90) || (97 <= x && x <= 122)){
+        std::cout << "Entered symbol is a letter" << std::endl;
         return 0;
     }
-    std::cout << "No Answer\nb exist in [0;7]\n";
+    else
+        std::cout <<"Entered symbol is unknown" << std::endl;
     return 0;
 }
-// 5
-int task5() {
-    unsigned short int d, c, e, f; // min 0 max 65535
-    unsigned short int answ;
-    std::cout << "Fifth task, insert c, d, e, f:\n";
-    std::cin >> d >> c >> e >> f;
-    answ=((~d|c)&e^f);
-    std::cout << "Answer: ";
-    std::cout << std::hex << std::setw(6) << std::setfill('0') << std::showbase << std::internal << answ << std::endl;
+
+int task3(){
+    std::cout << "Third task:" << std::endl;
+    unsigned int x;
+    std::cin >> x;
+    switch (x%4){
+        case 0:
+            std::cout << 0 << std::endl;
+            break;
+        case 1:
+            std::cout << 2*x+1 << std::endl;
+            break;
+        case 2:
+            std::cout << '-' << x << std::endl;
+            break;
+        case 3:
+            std::cout << 3*x << std::endl;
+            break;
+    }
+    return 0;
+}
+
+int task4(){
+    float x;
+    int power = -2;
+    std::cout << "Fourth task:" << std::endl;
+    std::cin >> x;
+    for ( unsigned int acc = 100; acc<=1000000; acc*=10) {
+        int n = 1;
+        float y = 1 / pow((1 - x), 3);
+        float answ = x;
+        while ((static_cast<int>(answ*acc)) != (static_cast<int>(y*acc))){
+            answ += (((n-1)*n)/2)*pow(x,(n-2));
+            n++;
+        }
+        std::cout << answ << " correctness 10^" << power << " iterations " << n << std::endl;
+        power--;
+    }
     return 0;
 }
